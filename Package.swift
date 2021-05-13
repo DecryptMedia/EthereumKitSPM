8d898b5559e8eb871019f89bbe7c7a37a48fefe5
Ryan Bubinski <ryanbubinski@gmail.com>
2021-05-13 12:46:56 -0700

    Use DecryptMedia forked CryptoEthereumSwiftSMP package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "EthereumKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS("12")
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,8 +17,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.2.0")),
-        .package(url: "https://github.com/cuhte3/CryptoEthereumSwiftSMP.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.4.0")),
+        .package(url: "https://github.com/DecryptMedia/CryptoEthereumSwiftSMP.git", .upToNextMajor(from: "2.0.3")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
